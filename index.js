8aeb65e53895d70411cb8e681a62047988f1b58c
te70 <42199000+te70@users.noreply.github.com>
2022-10-28 11:58:19 +0000

    Add files from replit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// var hngbio = mongoose.model('HngBio', hngbioSchema);
+
+// var hngbioSchema = new Schema({
+//   slackUsername: {type: String},
+//   backend: {type: Boolean},
+//   age : {type: Integer},
+//   bio: {type: String}
+// });
+
+app.get('/', (req, res) => {
+  return res.json({ String : "te70", Boolean :"backend", Number: "age", String : "Hi there"})
+});
+
+app.listen(3001, () => {
+  console.log('server started');
+});
